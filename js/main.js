3e106cdac5162cc5af5b90a2e5368b0911b45421
asdfendi <65590598+asdfendi@users.noreply.github.com>
2020-05-23 15:25:35 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//확장형 메뉴 동작-s
+$(document).ready(function(){
+  $("#menu").click(function(){
+    $("#hm").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".sd").click(function(){
+    $("#hm").toggle();
+  });
+});
+//확장형  메뉴 동작-e
